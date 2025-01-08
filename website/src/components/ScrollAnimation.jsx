e9fdb4c99a5b5c1c22414bb4e8abd1e3f08a1fbb
benjenner <benjaminjennersjo@gmail.com>
2025-01-08 14:50:48 +0100

    Additional animation

--- website/src/components/ScrollAnimation.jsx
+++ website/src/components/ScrollAnimation.jsx
@@ -1,11 +1,11 @@
-import React, { useEffect } from 'react';
-import { useInView } from 'react-intersection-observer';
-import { motion } from 'framer-motion';
+import React, { useEffect } from "react";
+import { useInView } from "react-intersection-observer";
+import { motion } from "framer-motion";
 
 const ScrollAnimation = ({ children }) => {
   const [ref, inView] = useInView({
-    threshold: 0.1, // När 10% av elementet syns
-    triggerOnce: false // Observera varje gång elementet kommer in i synfältet
+    threshold: 0.4, // När 10% av elementet syns
+    triggerOnce: false, // Observera varje gång elementet kommer in i synfältet
   });
 
   useEffect(() => {
@@ -18,7 +18,7 @@ const ScrollAnimation = ({ children }) => {
       initial={{ opacity: 0, y: 20 }}
       animate={inView ? { opacity: 1, y: 0 } : { opacity: 0, y: 20 }}
       exit={{ opacity: 0, y: 20 }}
-      transition={{ duration: 1 }}
+      transition={{ duration: 2 }}
     >
       {children}
     </motion.div>
